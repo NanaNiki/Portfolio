7a06cc66b2b5081652636dbe79eb99da02187ee4
NanaNiki <nicol.wesolowska@gmail.com>
2023-04-21 11:54:01 +0200

    "section contact added & sm icons"

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillMoonStarsFill, BsLinkedin, BsGithub } from "react-icons/bs";
 import { GiCrystalGrowth, GiAlienStare, GiUfo } from "react-icons/gi";
+import { FaFreeCodeCamp } from "react-icons/fa";
 import { IoMdPlanet } from "react-icons/io";
 import { useState } from "react";
 
@@ -38,17 +39,50 @@ export default function Home() {
           <h5>Fresh Baby Front End Developer</h5>
         </section>
         <section className="projects">
-          <h1>Here are my porjects</h1>
+          <h1>Here are my projects</h1>
         </section>
         <section className="about">
           <p>
             So far, I have been professionally involved in culture and creative
-            work in Poland and in international european environments.
-            <code> /!-- Biggest plot twist:--/</code> Art is by far the most
+            work in Poland and in international european environments.{" "}
+            <code>/!-- Biggest plot twist:--/</code> Art is by far the most
             indirect way of connection and delivery. With coding I can offer my
             skills and knolewledge, providing a desired product/help-tool. And
             what's even better it might end up being useful.
           </p>
+        </section>
+        <section className="contact">
+          <div>
+            <h3>Where to find me?</h3>
+            <BsLinkedin />
+            <BsGithub />
+            <FaFreeCodeCamp />
+          </div>
+          {/** message form */}
+          <form method="post">
+            <fieldset>
+              <label for="name" id="name-label">
+                Name
+                <input id="name" type="text" placeholder="Neo Smith" required />
+              </label>
+              <label for="email" id="email-label">
+                Email
+                <input
+                  id="email"
+                  type="email"
+                  required
+                  placeholder="oneneo@example.com"
+                />{" "}
+              </label>
+              <label for="message" id="message">
+                {" "}
+                <input placeholder="Your message"></input>
+              </label>
+              {/** here to change the submit form into email? or direct it to ok just research it :* */}
+              <input type="submit" id="submit" value="Submit" />
+            </fieldset>
+          </form>
+          {/** section for portfolio-sound-evaluation */}
         </section>
       </div>
     </main>
