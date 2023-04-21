cb477e972a26e695b5cea6b7af3c9e77aeaa4a42
NanaNiki <nicol.wesolowska@gmail.com>
2023-04-21 10:42:10 +0200

    "avatars added & head updated & hello stated"

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Nicol Wesołowska Portfolio</title>
+        <meta name="description" content="Portfolio app"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="">
+        <h1>Hello</h1>
+      </div>
     </main>
   );
 }
