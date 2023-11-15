2162aedf71501bc451b54b247f82b66570d3ec44
NanaNiki <nicol.wesolowska@gmail.com>
2023-11-15 22:15:02 +0100

    wibdow open email mobile

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -39,13 +39,12 @@ export default function Contact() {
         >
           <FaFreeCodeCamp className="icon animated-item slide-in-left delay-300 hover:delay-0" />
         </Link>
-        <a
-          href="mailto:nicol.wesolowska@gmail.com"
-          target="_blank"
+        <button
+          onClick={window.open('mailto:email@example.com', '_system')}
           aria-label="Write an email to me"
         >
           <SiGmail className="icon animated-item slide-in-left delay-100 hover:delay-0" />
-        </a>
+        </button>
         <div className="m-auto flex flex-row justify-end">
           <Link
             href="/CV_Nicol_Wesołowska.pdf"
