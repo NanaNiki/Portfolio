775a80c1c7ae4403d006688ec3f55ed168c11152
NanaNiki <nicol.wesolowska@gmail.com>
2023-11-15 23:30:14 +0100

    back to mail:to

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -39,13 +39,13 @@ export default function Contact() {
         >
           <FaFreeCodeCamp className="icon animated-item slide-in-left delay-300 hover:delay-0" />
         </Link>
-        <a
-          href="mailto://nicol.wesolowska@gmail.com"
+        <Link
+          href="mailto:nicol.wesolowska@gmail.com"
           target="_blank"
           aria-label="Write an email to me"
         >
           <SiGmail className="icon animated-item slide-in-left delay-100 hover:delay-0" />
-        </a>
+        </Link>
         <div className="m-auto flex flex-row justify-end">
           <Link
             href="/CV_Nicol_Wesołowska.pdf"
